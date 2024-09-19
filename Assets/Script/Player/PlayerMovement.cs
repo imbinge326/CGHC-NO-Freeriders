87397f824b8b5b4783ac5cabc973c382dbbfa790
Han040910 <135016878+Han040910@users.noreply.github.com>
2024-09-19 19:49:17 +0800

    mage attack done
    attack flip fix

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
-    private bool isFacingRight = true;
     [SerializeField] private float speed = 8f;
     [SerializeField] private float jumpingPower = 5f; 
     [SerializeField] private Rigidbody2D rb;
@@ -25,29 +24,17 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
-
-        Flip();
     }
 
     private void FixedUpdate()
     {
+        // Move the player without affecting the facing direction
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
-
-    private void Flip()
-    {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
     }
 }
 
