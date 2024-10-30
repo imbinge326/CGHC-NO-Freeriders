4bbb5e0eb87ea397dd77bc55f91a2007fb545973
imbinge326 <justinteh326@gmail.com>
2024-10-30 20:49:09 +0800

    leviathan boss

--- Assets/Script/Justin/Spawn LATwo.cs
+++ Assets/Script/Justin/Spawn LATwo.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class SpawnLATwo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> spawnPoints;
+    [SerializeField] private GameObject LATwo;
+    private float interval = 1f;
+    private GameObject point;
+    private int pointIndex = 0;
+
+        public void Start()
     {
-        
+        StartCoroutine(SpawnLAONE());
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator SpawnLAONE()
     {
-        
+        while (true)
+        {
+            GetPoints();
+            point = spawnPoints[pointIndex];
+            yield return new WaitForSeconds(interval);
+            FireLATWO();
+        }
     }
+    private void FireLATWO()
+    {
+        Instantiate(LATwo, point.gameObject.transform.position, Quaternion.Euler(0,0,-90));
+    }
+
+    private int GetPoints()
+    {
+        if (pointIndex < spawnPoints.Count - 1)
+        {
+            return pointIndex++;
+        }
+        else
+        {
+            return pointIndex = 0;
+        }
+    }
+
 }
